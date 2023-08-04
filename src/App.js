c81116b0849e397733f251134137e6065c89db96
cwvilla770 <hello@chriswilson.work>
2023-08-04 10:17:33 +0300

    testingfromconsolecode

--- src/App.js
+++ src/App.js
@@ -1,107 +1,47 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import "@aws-amplify/ui-react/styles.css";
 import { API } from "aws-amplify";
-import {
-  Button,
-  Flex,
-  Heading,
-  Text,
-  TextField,
-  View,
-  withAuthenticator,
-} from "@aws-amplify/ui-react";
-import { listNotes } from "./graphql/queries";
-import {
-  createNote as createNoteMutation,
-  deleteNote as deleteNoteMutation,
-} from "./graphql/mutations";
-
-const App = ({ signOut }) => {
-  const [notes, setNotes] = useState([]);
-
-  useEffect(() => {
-    fetchNotes();
-  }, []);
-
-  async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
-    setNotes(notesFromAPI);
+import { createNote } from './graphql/mutations';
+const newNote = await API.graphql({
+  query: createNote,
+  variables: {
+      input: {
+  "name": "Lorem ipsum dolor sit amet",
+  "description": "Lorem ipsum dolor sit amet"
+}
   }
+});
 
-  async function createNote(event) {
-    event.preventDefault();
-    const form = new FormData(event.target);
-    const data = {
-      name: form.get("name"),
-      description: form.get("description"),
-    };
-    await API.graphql({
-      query: createNoteMutation,
-      variables: { input: data },
-    });
-    fetchNotes();
-    event.target.reset();
+import { API } from "aws-amplify";
+import { updateNote } from './graphql/mutations';
+const updatedNote = await API.graphql({
+  query: updateNote,
+  variables: {
+      input: {
+  "name": "Lorem ipsum dolor sit amet",
+  "description": "Lorem ipsum dolor sit amet"
+}
   }
+});
 
-  async function deleteNote({ id }) {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-    await API.graphql({
-      query: deleteNoteMutation,
-      variables: { input: { id } },
-    });
+import { API } from "aws-amplify";
+import { deleteNote } from './graphql/mutations';
+const deletedNote = await API.graphql({
+  query: deleteNote,
+  variables: {
+      input: {
+          id: "YOUR_RECORD_ID"
+      }
   }
+});
 
-  return (
-    <View className="App">
-      <Heading level={1}>My Notes App1</Heading>
-      <View as="form" margin="3rem 0" onSubmit={createNote}>
-        <Flex direction="row" justifyContent="center">
-          <TextField
-            name="name"
-            placeholder="Note Name"
-            label="Note Name"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <TextField
-            name="description"
-            placeholder="Note Description"
-            label="Note Description"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <Button type="submit" variation="primary">
-            Create Note
-          </Button>
-        </Flex>
-      </View>
-      <Heading level={2}>Current Notes</Heading>
-      <View margin="3rem 0">
-        {notes.map((note) => (
-          <Flex
-            key={note.id || note.name}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text as="strong" fontWeight={700}>
-              {note.name}
-            </Text>
-            <Text as="span">{note.description}</Text>
-            <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
-            </Button>
-          </Flex>
-        ))}
-      </View>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
-  );
-};
-
-export default withAuthenticator(App);
+import { API } from "aws-amplify";
+import { listNotes, getNote } from "./graphql/queries";
+// List all items
+const allNotes = await API.graphql({
+  query: listNotes
+});
+console.log(allNote);
+// Get a specific item
+const oneNote = await API.graphql({
+  query: getNote,
+  variables: { id: 'YOUR_RECORD_ID' }
+});
